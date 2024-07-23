41d99eaa8df75d956efcaedc0ace3a2ff13f1714
imaxwellperry <maxwell-perry.i@northeastern.edu>
2024-07-22 21:46:22 -0400

    Added Weapon Stuff

---
+++ Scripts/WeaponBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Chop()
+    {
+        //GetComponent<Animator>().SetTrigger("WeaponSwung");
+    }
+}
